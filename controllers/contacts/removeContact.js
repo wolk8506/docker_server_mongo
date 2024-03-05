23930a5dabab2ed1a0e11c5ba12762019b44781f
Yuriy Krasyukov <89795245+wolk8506@users.noreply.github.com>
2024-03-05 14:27:24 +0200

    first commit

---
+++ controllers/contacts/removeContact.js
@@ -0,0 +1,20 @@
+const { Contact } = require("../../models");
+const createError = require("http-errors");
+
+const removeContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndRemove(contactId);
+  if (!result) {
+    throw createError(404, `Contact with id=${contactId} not found`);
+  }
+  res.json({
+    status: "success",
+    code: 200,
+    message: "Contact deleted",
+    data: {
+      result,
+    },
+  });
+};
+
+module.exports = removeContact;
---
+++ controllers/contacts/updateContact.js
@@ -0,0 +1,21 @@
+const { Contact } = require("../../models");
+const createError = require("http-errors");
+
+const updateContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw createError(404, `Contact with id=${contactId} not found`);
+  }
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result,
+    },
+  });
+};
+
+module.exports = updateContact;
