38ce0b327dee5d0db85618552b43810c8d99711e
Yuriy Krasyukov <89795245+wolk8506@users.noreply.github.com>
2024-03-05 16:44:38 +0200

    fix7

--- bin/server.js
+++ bin/server.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const app = require("../app");
 
-// const { DB_HOST = "mongodb://127.0.0.1:27019/", PORT = 4000 } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
-  .connect("mongodb://0.0.0.0:27017/")
+  .connect(DB_HOST)
   .then(() => console.log("Database connection successful"))
   .then(() =>
-    app.listen(4000, () => {
-      console.log(`Server running. Use our API on port: ${4000}`);
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
     })
   )
   .catch((error) => {
